7df3937a6ae708bca78b7e3bfbc6b96eb27defa1
willianfreiberger-boop <willian.freiberger@escola.pr.gov.br>
2026-07-09 14:57:02 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,12 @@
+const avanca = document.querySelectorAll('.btn-proximo');
+
+avanca.forEach(button => {
+    button.addEventListener('click', function(){
+        const atual = document.querySelector('.ativo');
+        const proximoPasso = 'passo-' + this.getAttribute('data-proximo');
+
+        atual.classList.remove('ativo');
+        const proximoElemento = document.getElementById(proximoPasso);
+        proximoElemento.classList.add('ativo');
+    })
+});
